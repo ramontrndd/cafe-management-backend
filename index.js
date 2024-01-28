b8a55a87173457d01616898740970a63a582bd81
Ramon Trindade <ramonbraintrindade@gmail.com>
2024-01-28 12:18:33 -0300

    feat: add category products

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var cors = require('cors');
 
 const connection = require('./connection');
 const userRoute = require('./routes/user')
+const categoryRoute = require('./routes/category')
 const app = express();
 
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/user', userRoute)
+app.use('/user', userRoute);
+app.use('/category', categoryRoute)
 
 module.exports = app;
