4dc300aa16150d609f85fb685d498bc3dcd72c47
Ramon Trindade <ramonbraintrindade@gmail.com>
2024-02-01 11:23:18 -0300

    feat: API dashboard

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const connection = require('./connection');
 const userRoute = require('./routes/user')
 const categoryRoute = require('./routes/category')
 const productRoute = require('./routes/product');
-const billRoute = require('./routes/bill')
+const billRoute = require('./routes/bill');
+const dashboardRoute = require('./routes/dashboard');
 const app = express();
 
 
@@ -16,5 +17,7 @@ app.use('/user', userRoute);
 app.use('/category', categoryRoute);
 app.use('/product', productRoute);
 app.use('/bill', billRoute);
+app.use('/dashboard', dashboardRoute);
+
 
 module.exports = app;
