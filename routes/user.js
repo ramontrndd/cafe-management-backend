b8795fab57dd10519bf22e5b94c1c0ba9cd18c90
Ramon Trindade <ramonbraintrindade@gmail.com>
2024-03-19 13:50:25 -0300

    fix: nodemailer errors

--- routes/user.js
+++ routes/user.js
@@ -75,8 +75,8 @@ var transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
   },
 });
 
