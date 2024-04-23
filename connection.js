ddee564eec53f777e71fc8436f2ac973d0665ba5
Ramon Trindade <ramonbraintrindade@gmail.com>
2024-04-23 14:32:45 -0300

    update: connect.js

--- connection.js
+++ connection.js
@@ -1,20 +1,46 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-var connection = mysql.createConnection({
-  port: process.env.DB_PORT,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+let connection;
 
-connection.connect((err) => {
-  if (!err) {
-    console.log("Conectado");
-  } else {
-    console.log(err);
+function connectionDB() {
+  connection = mysql.createConnection({
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+
+  connection.connect((err) => {
+    if (!err) {
+      console.log("Conectado");
+    } else {
+      console.error("Erro ao conectar:", err);
+      reconnect();
+    }
+  });
+}
+
+function reconnect() {
+  const tryMax = 3;
+  let tentativa = 0;
+
+  function retryConnection() {
+    if (tentativa < tryMax) {
+      tentativa++;
+      console.log(`Tentando reconectar... Tentativa ${tentativa}`);
+      connectionDB();
+    } else {
+      console.error(
+        "Número máximo de tentativas atingido. Não foi possível reconectar."
+      );
+    }
   }
-});
+
+  setTimeout(retryConnection, 5000);
+}
+
+connectionDB();
 
 module.exports = connection;
